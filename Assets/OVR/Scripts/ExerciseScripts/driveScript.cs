bf9d70bc8bbb3c7dc86215539ff707c33c9e98c3
Andreas Rabouli <andreas.h@rabouli.com>
2018-03-19 13:52:43 +0100

    Initial commit

---
+++ Assets/OVR/Scripts/ExerciseScripts/driveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driveScript : MonoBehaviour {
+
+    public float speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)) {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+
+        Vector2 res = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+
+        // Left
+        if (res.x < 0)
+        {
+            transform.Rotate(0, -1f, 0);
+        }
+        else if (res.x > 0) // Right
+        {
+            transform.Rotate(0, 1f, 0);
+        }
+
+	}
+}
